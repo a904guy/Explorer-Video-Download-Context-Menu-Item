dc135695a181a86885d155809a21afe46e6595ea
Andy Hawkins — (̖̣͕̠̬̭̞̪̐̅ỉ̭̣͈̪̠ͮs̳̳̦̞̰̜̞̳͑͌͋ͪ̃̍ ̩̘́͑ͨ̈́̎̋ͮ͊̀ñ͔̣͓̬̈ͮͦ́ͫ͐͛ͅͅo̩̦̳̠̙̗̯̺̐͆̽t̜̥̻̙̟̼̜͂ͩ͋̅̈́ ̠̦̹̟͕͕̱͉͒͋̅́͐ā̟̻̱ͦ̒̍͋ ̗̞͚ͨͩ͊͂̉ͅh͔̼͚̩͚̪̝̝ͣa̫̫̒̓ͬc̦̲̳͍̹̃ͥͯk̲͕̗̖̤̙̜͍̊e̫͎̟̼̺̫̿r͙̰̊̑̿̅ͯͅ)̪̖͇̝̮̪͖̦͆̇ͧ͑̚ <a904guy@users.noreply.github.com>
2024-03-07 12:03:44 -0800

    Update video_context_menu.py

--- video_context_menu.py
+++ video_context_menu.py
@@ -45,6 +45,17 @@ def add_to_context_menu():
 
     reg.SetValue(command_key, '', reg.REG_SZ, f'{sys.executable} "{application_path}" "%V"')
     reg.CloseKey(command_key)
+
+    def install_ffmpeg():
+        subprocess.run(["winget", "install", "ffmpeg"])
+
+    try:
+        result = subprocess.run(["ffmpeg", "-version"], capture_output=True, text=True)
+        if result.returncode != 0:
+            install_ffmpeg()
+    except OSError as e:
+        install_ffmpeg()
+
     print("Context menu item added.")
 
 # Function to display the URL input dialog and download the video
@@ -54,20 +65,15 @@ def download_video_dialog(directory):
     video_url = simpledialog.askstring("Download Video", "Enter the Video URL:")
     
     if video_url:
-        # Change to the directory where the context menu was clicked
         os.chdir(directory)
-        # Assuming yt-dlp is installed and added to PATH
         subprocess.run(["yt-dlp", video_url])
 
         print('You can safely close this window now.')
         input("Press Enter to exit...")
 
-# Main execution logic
 if __name__ == "__main__":
-    # Check if "install" is in the command line arguments
     if len(sys.argv) > 1 and sys.argv[1] == "--install":
         add_to_context_menu()
     else:
-        # The directory path should be the second argument, if present
         directory = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
         download_video_dialog(directory)
